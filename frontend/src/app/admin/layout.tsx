76c9915a6d589c1b7211f2aaa9914142630abd9c
Adharsh S <adharshkattaikonam@gmail.com>
2025-11-21 10:46:42 +0530

    feat/google-auth: After completing google auth admin layout setup complete

---
+++ frontend/src/app/admin/layout.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React, { useState } from "react";
+import Sidebar from "@/components/containers/AdminSidebar"; 
+import Navbar from "@/components/containers/AdminNavbar"; 
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
+
+  return (
+    <div className="h-screen bg-[#f3f4f6] flex overflow-hidden">
+      <Sidebar isOpen={sidebarOpen} />
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <Navbar onMenuClick={toggleSidebar} sidebarOpen={sidebarOpen} />
+        <main className="flex-1 overflow-auto">{children}</main>
+      </div>
+    </div>
+  );
+}
